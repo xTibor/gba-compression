bb50c9311f80ac8509637e1afc5e7ac0f55a705d
Tibor Nagy <xnagytibor@gmail.com>
2017-05-08 18:56:59 +0200

    Work in progress LZ77 compression

--- bios/lz77.rs
+++ bios/lz77.rs
@@ -1,6 +1,16 @@
 use std::io::{Read, Write, Result, Error, ErrorKind};
-use byteorder::{LittleEndian, ReadBytesExt};
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use compression::bios::{BiosCompressionType, bios_compression_type};
+
+enum Block {
+    Uncompressed {
+        data: u8,
+    },
+    Backreference {
+        offset: u16,
+        length: u8,
+    }
+}
 
 /// Decompression routine for GBA BIOS LZ77 encoded data
 #[allow(dead_code)]
@@ -49,8 +59,47 @@ pub fn decompress_lz77<R: Read, W: Write>(input: &mut R, output: &mut W) -> Resu
 }
 
 #[allow(dead_code)]
-pub fn compress_lz77<R: Read, W: Write>(_input: &mut R, _output: &mut W) -> Result<()> {
-    Err(Error::new(ErrorKind::Other, "Unimplemented compression routine"))
+pub fn compress_lz77<R: Read, W: Write>(input: &mut R, output: &mut W) -> Result<()> {
+    let mut buffer: Vec<u8> = Vec::new();
+    input.read_to_end(&mut buffer)?;
+
+    let mut blocks: Vec<Block> = Vec::new();
+    let mut offset = 0;
+    while offset < buffer.len() {
+        // TODO: Find backreferences
+        blocks.push(Block::Uncompressed { data: buffer[offset] });
+
+        offset += 1;
+    }
+
+    output.write_u8((BiosCompressionType::Lz77 as u8) << 4)?;
+    output.write_u24::<LittleEndian>(buffer.len() as u32)?;
+
+    for chunk in blocks.chunks(8) {
+        let mut block_types = 0;
+        for (i, block) in chunk.iter().enumerate() {
+            if let Block::Backreference { .. } = *block  {
+                block_types |= 0x80 >> i;
+            }
+        }
+        output.write_u8(block_types)?;
+
+        for block in chunk {
+            match *block {
+                Block::Uncompressed { data } => {
+                    output.write_u8(data)?;
+                },
+                Block::Backreference { offset, length } => {
+                    assert!((length >= 3) & (length <= 18), "length must be between 3 and 18");
+                    assert_eq!(offset >> 12, 0, "offset must be 12-bit");
+                    let data = ((offset & 0xFF) << 8) | ((length - 3) << 4) as u16 | (offset >> 8);
+                    output.write_u16::<LittleEndian>(data)?;
+                },
+            }
+        }
+    }
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -95,5 +144,40 @@ mod tests {
 
     #[test]
     fn test_compress_1() {
+        let input: Vec<u8> = Vec::new();
+        let expected_output: Vec<u8> = vec![
+            0x10, 0x00, 0x00, 0x00,
+        ];
+
+        let mut output: Vec<u8> = Vec::new();
+        compress_lz77(&mut Cursor::new(&input[..]), &mut output).unwrap();
+        assert_eq!(output, expected_output);
+    }
+
+    #[test]
+    fn test_compress_and_decompress_1() {
+        let input: Vec<u8> = Vec::new();
+
+        let mut immediate: Vec<u8> = Vec::new();
+        let mut output: Vec<u8> = Vec::new();
+        compress_lz77(&mut Cursor::new(&input[..]), &mut immediate).unwrap();
+        decompress_lz77(&mut Cursor::new(&immediate[..]), &mut output).unwrap();
+        assert_eq!(input, output);
+    }
+
+    #[test]
+    fn test_compress_and_decompress_2() {
+        let input: Vec<u8> = vec![
+            0x01, 0x02, 0x03, 0x04,
+            0x05, 0x05, 0x05, 0x05,
+            0x01, 0x02, 0x03, 0x04,
+            0x05, 0x05, 0x05, 0x05,
+        ];
+
+        let mut immediate: Vec<u8> = Vec::new();
+        let mut output: Vec<u8> = Vec::new();
+        compress_lz77(&mut Cursor::new(&input[..]), &mut immediate).unwrap();
+        decompress_lz77(&mut Cursor::new(&immediate[..]), &mut output).unwrap();
+        assert_eq!(input, output);
     }
 }
