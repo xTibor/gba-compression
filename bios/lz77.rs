e2cf817311a6c9ebf02556103e840222a952174e
Tibor Nagy <xnagytibor@gmail.com>
2017-10-03 15:14:40 +0200

    Add a VRAM-safe option to the LZ77 compressor

--- bios/lz77.rs
+++ bios/lz77.rs
@@ -5,16 +5,8 @@ use compression::Compressor;
 use compression::bios::{BiosCompressionType, bios_compression_type};
 
 #[derive(Default)]
-pub struct Lz77Compressor;
-
-enum Block {
-    Uncompressed {
-        data: u8,
-    },
-    Backreference {
-        offset: u16,
-        length: u8,
-    }
+pub struct Lz77Compressor {
+    pub vram_safe: bool,
 }
 
 impl Compressor for Lz77Compressor {
@@ -40,18 +32,18 @@ impl Compressor for Lz77Compressor {
                         // Backreference
                         let block = cursor.read_u16::<LittleEndian>()? as usize;
                         let length = ((block >> 4) & 0xF) + 3;
-                        let offset = ((block & 0xF) << 8) | ((block >> 8) & 0xFF);
+                        let offset = (((block & 0xF) << 8) | ((block >> 8) & 0xFF)) + 1;
 
                         if buffer.len() + length > decompressed_size {
                             return Err(Error::new(ErrorKind::InvalidData, "Length out of bounds"));
                         }
 
-                        if offset + 1 > buffer.len() {
+                        if offset > buffer.len() {
                             return Err(Error::new(ErrorKind::InvalidData, "Offset out of bounds"));
                         }
 
                         for _ in 0..length {
-                            let index = buffer.len() - offset - 1;
+                            let index = buffer.len() - offset;
                             let byte = buffer[index];
                             buffer.push(byte);
                         }
@@ -65,15 +57,27 @@ impl Compressor for Lz77Compressor {
     }
 
     fn compress(&self, input: &[u8], output: &mut Vec<u8>) -> Result<()> {
+        enum Block {
+            Uncompressed {
+                data: u8,
+            },
+            Backreference {
+                offset: u16,
+                length: u8,
+            }
+        }
+
         let mut blocks: Vec<Block> = Vec::new();
         let mut index = 0;
 
         'outer: while index < input.len() {
             let block_max_length = cmp::min(input.len() - index, 18);
             for length in (3..block_max_length + 1).rev() {
-                // TODO: Is overlapping tolerated by the BIOS decompression routine?
-                // The `offset` counter has to start at `length` if not.
-                let mut offset = 1;
+                // When decompressing to VRAM the previous byte cannot be referenced in
+                // the uncompressed data because it may have not written to the memory yet.
+                // The data to the VRAM is written in 16-bit words due to 16-bit data bus.
+                let mut offset = if self.vram_safe { 2 } else { 1 };
+
                 while (offset <= 4096) && (index >= offset) {
                     let index_back = index - offset;
                     if input[index..index + length] == input[index_back..index_back + length] {
@@ -110,7 +114,12 @@ impl Compressor for Lz77Compressor {
                     },
                     Block::Backreference { offset, length } => {
                         assert!((length >= 3) & (length <= 18), "length must be between 3 and 18");
-                        assert!((offset >= 1) & (offset <= 4096), "offset must be between 1 and 4096");
+                        if self.vram_safe {
+                            assert!((offset >= 2) & (offset <= 4096), "offset must be between 2 and 4096");
+                        } else {
+                            assert!((offset >= 1) & (offset <= 4096), "offset must be between 1 and 4096");
+                        }
+
                         let data = (((offset - 1) & 0xFF) << 8) | ((length - 3) << 4) as u16 | ((offset - 1) >> 8);
                         output.write_u16::<LittleEndian>(data)?;
                     },
@@ -232,6 +241,34 @@ mod tests {
             0x05, 0x06, 0x07, 0x08,
             0x02, 0x03, 0x01, 0x02,
         ];
+
+        let mut immediate: Vec<u8> = Vec::new();
+        let mut output: Vec<u8> = Vec::new();
+        let compressor = Lz77Compressor::default();
+
+        compressor.compress(&input, &mut immediate).unwrap();
+        compressor.decompress(&immediate, &mut output).unwrap();
+        assert_eq!(input, output);
+    }
+
+    #[test]
+    fn test_compress_vram_safe_1() {
+        let input: Vec<u8> = vec![0xFF; 16];
+        let expected_output: Vec<u8> = vec![
+            0x10, 0x10, 0x00, 0x00,
+            0x20, 0xFF, 0xFF, 0xB0, 0x01
+        ];
+
+        let mut output: Vec<u8> = Vec::new();
+        let compressor = Lz77Compressor { vram_safe: true };
+
+        compressor.compress(&input, &mut output).unwrap();
+        assert_eq!(output, expected_output);
+    }
+
+    #[test]
+    fn test_compress_vram_safe_2() {
+        let input: Vec<u8> = vec![0xFF; 4096];
 
         let mut immediate: Vec<u8> = Vec::new();
         let mut output: Vec<u8> = Vec::new();
