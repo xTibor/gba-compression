764dd6f288def881c7df1103e9d9dcb023a3c091
Tibor Nagy <xnagytibor@gmail.com>
2017-10-06 01:49:58 +0200

    Use only the trait name instead of the full path

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-pub fn consecutive_count<T: ::std::cmp::Eq>(buffer: &[T], max_length: usize) -> usize {
+pub fn consecutive_count<T: Eq>(buffer: &[T], max_length: usize) -> usize {
     let mut count = 0;
     while (count < buffer.len()) && (count < max_length) && (buffer[0] == buffer[count]) {
         count += 1;
@@ -6,7 +6,7 @@ pub fn consecutive_count<T: ::std::cmp::Eq>(buffer: &[T], max_length: usize) -> 
     count
 }
 
-pub fn non_consecutive_count<T: ::std::cmp::Eq>(buffer: &[T], max_length: usize, consecutive_threshold: usize) -> usize {
+pub fn non_consecutive_count<T: Eq>(buffer: &[T], max_length: usize, consecutive_threshold: usize) -> usize {
     let mut count = 0;
     while (count < buffer.len()) && (count < max_length) && (consecutive_count(&buffer[count..], max_length) < consecutive_threshold) {
         count += 1;
