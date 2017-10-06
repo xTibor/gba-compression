8e89647da8f0214d7231fca1787a5857259500ee
Tibor Nagy <xnagytibor@gmail.com>
2017-10-06 16:17:49 +0200

    Make tests compact

--- src/utils.rs
+++ src/utils.rs
@@ -19,74 +19,22 @@ mod tests {
     use utils::{consecutive_count, non_consecutive_count};
 
     #[test]
-    fn test_consecutive_count_1() {
-        let input: Vec<u8> = Vec::new();
-        assert_eq!(consecutive_count(&input, 256), 0);
+    fn test_consecutive_count() {
+        assert_eq!(consecutive_count::<u8>(&vec![                            ], 9), 0);
+        assert_eq!(consecutive_count::<u8>(&vec![0x00                        ], 9), 1);
+        assert_eq!(consecutive_count::<u8>(&vec![0x00, 0x00, 0x00, 0x00      ], 9), 4);
+        assert_eq!(consecutive_count::<u8>(&vec![0x00, 0x00, 0x00, 0x00, 0x01], 9), 4);
+        assert_eq!(consecutive_count::<u8>(&vec![0x00, 0x00, 0x00, 0x00      ], 3), 3);
+        assert_eq!(consecutive_count::<u8>(&vec![0x00, 0x00                  ], 3), 2);
+        assert_eq!(consecutive_count::<u8>(&vec![0x00, 0x00, 0x01            ], 3), 2);
     }
 
     #[test]
-    fn test_consecutive_count_2() {
-        let input: Vec<u8> = vec![0x00];
-        assert_eq!(consecutive_count(&input, 256), 1);
-    }
-
-    #[test]
-    fn test_consecutive_count_3() {
-        let input: Vec<u8> = vec![0x00, 0x00, 0x00, 0x00];
-        assert_eq!(consecutive_count(&input, 256), 4);
-    }
-
-    #[test]
-    fn test_consecutive_count_4() {
-        let input: Vec<u8> = vec![0x00, 0x00, 0x00, 0x00, 0x01];
-        assert_eq!(consecutive_count(&input, 256), 4);
-    }
-
-    #[test]
-    fn test_consecutive_count_5() {
-        let input: Vec<u8> = vec![0x00, 0x00, 0x00, 0x00];
-        assert_eq!(consecutive_count(&input, 3), 3);
-    }
-
-    #[test]
-    fn test_consecutive_count_6() {
-        let input: Vec<u8> = vec![0x00, 0x00];
-        assert_eq!(consecutive_count(&input, 3), 2);
-    }
-
-    #[test]
-    fn test_consecutive_count_7() {
-        let input: Vec<u8> = vec![0x00, 0x00, 0x01];
-        assert_eq!(consecutive_count(&input, 3), 2);
-    }
-
-    #[test]
-    fn test_non_consecutive_count_1() {
-        let input: Vec<u8> = Vec::new();
-        assert_eq!(non_consecutive_count(&input, 256, 2), 0);
-    }
-
-    #[test]
-    fn test_non_consecutive_count_2() {
-        let input: Vec<u8> = vec![0x00, 0x01, 0x02, 0x03, 0x04, 0x05];
-        assert_eq!(non_consecutive_count(&input, 256, 2), 6);
-    }
-
-    #[test]
-    fn test_non_consecutive_count_3() {
-        let input: Vec<u8> = vec![0x00, 0x00, 0x01, 0x01, 0x02, 0x02];
-        assert_eq!(non_consecutive_count(&input, 256, 3), 6);
-    }
-
-    #[test]
-    fn test_non_consecutive_count_4() {
-        let input: Vec<u8> = vec![0x00, 0x00, 0x01, 0x01, 0x02, 0x02];
-        assert_eq!(non_consecutive_count(&input, 3, 3), 3);
-    }
-
-    #[test]
-    fn test_non_consecutive_count_5() {
-        let input: Vec<u8> = vec![0x00, 0x00, 0x00, 0x01, 0x01, 0x01];
-        assert_eq!(non_consecutive_count(&input, 256, 3), 0);
+    fn test_non_consecutive_count() {
+        assert_eq!(non_consecutive_count::<u8>(&vec![                                  ], 9, 2), 0);
+        assert_eq!(non_consecutive_count::<u8>(&vec![0x00, 0x01, 0x02, 0x03, 0x04, 0x05], 9, 2), 6);
+        assert_eq!(non_consecutive_count::<u8>(&vec![0x00, 0x00, 0x01, 0x01, 0x02, 0x02], 9, 3), 6);
+        assert_eq!(non_consecutive_count::<u8>(&vec![0x00, 0x00, 0x01, 0x01, 0x02, 0x02], 3, 3), 3);
+        assert_eq!(non_consecutive_count::<u8>(&vec![0x00, 0x00, 0x00, 0x01, 0x01, 0x01], 9, 3), 0);
     }
 }
